cc7f96ecd8c643bd5ccc375853c22416019728d7
lihaolun.vendor <lihaolun.vendor@sensetime.com>
2022-05-16 06:11:15 +0000

    add

--- algolib/runner/train.sh
+++ algolib/runner/train.sh
@@ -1,92 +1,79 @@
 #!/bin/bash
 set -x
+set -o pipefail
+set -e
 
+# 0. check the most important SMART_ROOT
+echo  "!!!!!SMART_ROOT is" $SMART_ROOT
 if $SMART_ROOT; then
     echo "SMART_ROOT is None,Please set SMART_ROOT"
     exit 0
 fi
 
-if [ -x "$SMART_ROOT/submodules" ];then
-    submodules_root=$SMART_ROOT
-else    
-    submodules_root=$PWD
-fi
-
-if [[ "$submodules_root" =~ "submodules/mmtrack" ]]
-then
-    if [ ! -d "$submodules_root/../mmdet/mmdet" ]
+# 1. set env_path and build soft links for mm configs
+if [[ $PWD =~ "mmtrack" ]]
+then 
+    pyroot=$PWD
+    if [ ! -d "$PWD/../mmdet/mmdet" ]
     then
-        cd ../..
-        git submodule update --init submodules/mmdet
+        cd ..
+        git submodule update --init mmdet
         cd -
     fi
-    if [ ! -d "$submodules_root/../mmcls/mmcls" ]
+    if [ ! -d "$PWD/../mmcls/mmcls" ]
     then
-        cd ../..
-        git submodule update --init submodules/mmcls
+        cd ..
+        git submodule update --init mmcls
         cd -
     fi
 else
-    if [ ! -d "$submodules_root/submodules/mmdet/mmdet" ]
+    pyroot=$PWD/mmtrack
+    if [ ! -d "$PWD/mmdet/mmdet" ]
     then
-        git submodule update --init submodules/mmdet
+        git submodule update --init mmdet
     fi
-    if [ ! -d "$submodules_root/submodules/mmcls/mmcls" ]
+    if [ ! -d "$PWD/mmcls/mmcls" ]
     then
-        git submodule update --init submodules/mmcls
+        git submodule update --init mmcls
     fi
-fi
-
-# 0. placeholder
-if [ -d "$submodules_root/submodules/mmtrack/algolib/configs" ]
-then
-    rm -rf $submodules_root/submodules/mmtrack/algolib/configs
-    ln -s $submodules_root/submodules/mmtrack/configs $submodules_root/submodules/mmtrack/algolib/
-else
-    ln -s $submodules_root/submodules/mmtrack/configs $submodules_root/submodules/mmtrack/algolib/
-fi
- 
-# 1. build file folder for save log,format: algolib_gen/frame
-mkdir -p algolib_gen/mmtrack/$3
-export PYTORCH_VERSION=1.4
- 
-# 2. set time
-now=$(date +"%Y%m%d_%H%M%S")
- 
-# 3. set env
-path=$PWD
-if [[ "$path" =~ "submodules" ]]
-then
-    pyroot=$submodules_root/mmtrack
-else
-    pyroot=$submodules_root/submodules/mmtrack
 fi
 echo $pyroot
+if [ -d "$pyroot/algolib/configs" ]
+then
+    rm -rf $pyroot/algolib/configs
+    ln -s $pyroot/configs $pyroot/algolib/
+else
+    ln -s $pyroot/configs $pyroot/algolib/
+fi
+ 
+# 2. build file folder for save log,format: algolib_gen/frame
+mkdir -p algolib_gen/mmtrack/$3
+now=$(date +"%Y%m%d_%H%M%S")
+ 
+# 3. set env variables
+export PYTORCH_VERSION=1.4
 export PYTHONPATH=$pyroot:$PYTHONPATH
 export FRAME_NAME=mmtrack    #customize for each frame
 export MODEL_NAME=$3
+export PARROTS_DEFAULT_LOGGER=FALSE
 
-# mmdet_path and mmcls_path
-SHELL_PATH=$(dirname $0)
-export PYTHONPATH=$SHELL_PATH/../../../mmdet:$PYTHONPATH
-export PYTHONPATH=$SHELL_PATH/../../../mmcls:$PYTHONPATH
+# 4. mmdetpath and init_path
+export PYTHONPATH=$pyroot/../mmdet:$PYTHONPATH
+export PYTHONPATH=$pyroot/../mmcls:$PYTHONPATH
+export PYTHONPATH=${SMART_ROOT}:$PYTHONPATH
+export PYTHONPATH=$SMART_ROOT/common/sites/:$PYTHONPATH
 
-# init_path
-export PYTHONPATH=$SMART_ROOT/common/sites/:$PYTHONPATH 
- 
-# 4. build necessary parameter
+# 5. build necessary parameter
 partition=$1 
 name=$3
 MODEL_NAME=$3
 g=$(($2<8?$2:8))
 array=( $@ )
-len=${#array[@]}
-EXTRA_ARGS=${array[@]:3:$len}
+EXTRA_ARGS=${array[@]:3}
+EXTRA_ARGS=${EXTRA_ARGS//--resume/--resume-from}
 SRUN_ARGS=${SRUN_ARGS:-""}
  
-# 5. model choice
-export PARROTS_DEFAULT_LOGGER=FALSE
-
+# 6. model list
 case $MODEL_NAME in
     "dff_faster_rcnn_r101_dc5_1x_imagenetvid")
         FULL_MODEL="vid/dff/dff_faster_rcnn_r101_dc5_1x_imagenetvid"
@@ -118,11 +105,12 @@ case $MODEL_NAME in
        ;; 
 esac
 
+# 7. set port and choice model
 port=`expr $RANDOM % 10000 + 20000`
-
 file_model=${FULL_MODEL##*/}
 folder_model=${FULL_MODEL%/*}
 
+# 8. run model
 srun -p $1 -n$2\
         --gres gpu:$g \
         --ntasks-per-node $g \
